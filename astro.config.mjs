5e4e90bdba4c9f53b9cda406ed648b1b1bc51ba7
Michael Price <michael@michamp.com>
2024-06-19 16:04:27 -0700

    Initial SWAT API V1 template

---
+++ astro.config.mjs
@@ -0,0 +1,20 @@
+import { defineConfig } from 'astro/config';
+import react from '@astrojs/react';
+import icon from "astro-icon";
+
+import tailwind from "@astrojs/tailwind";
+
+// https://astro.build/config
+export default defineConfig({
+  // Enable React to support React JSX components.
+  integrations: [react(), icon({
+    include: {
+      // Include `iconoir` icons in the bundle
+      iconoir: ['network-reverse', 'view-structure-down', 'cloud-sync', 'electronics-chip', 'database', 'code'],
+      // Include `carbon` icons in the bundle
+      carbon: ['api-1', 'container-services', 'dashboard', 'app-connectivity', 'add', 'email', 'password', 'login', 'error', 'logout'],
+      // Include `remix` icons in the bundle
+      ri: ['arrow-down-double-fill', 'arrow-down-circle-line', 'close-circle-line', 'mail-line', 'key-fill', 'menu-fill', 'add-box-line', 'file-copy-2-line', 'settings-5-line', 'user-add-line', 'chat-new-line', 'code-box-line', 'menu-2-fill', 'file-list-3-line', 'user-add-line','home-2-line','user-3-line'],
+    }
+  }), tailwind()]
+});
