996d73678d97295da065db6da4ab4b943c423587
Michael Price <michael@michamp.com>
2024-06-19 17:35:01 -0700

    Initial SWAT API V1 template

--- astro.config.mjs
+++ astro.config.mjs
@@ -14,7 +14,7 @@ export default defineConfig({
       // Include `carbon` icons in the bundle
       carbon: ['api-1', 'container-services', 'dashboard', 'app-connectivity', 'add', 'email', 'password', 'login', 'error', 'logout'],
       // Include `remix` icons in the bundle
-      ri: ['arrow-down-double-fill', 'arrow-down-circle-line', 'close-circle-line', 'mail-line', 'key-fill', 'menu-fill', 'add-box-line', 'file-copy-2-line', 'settings-5-line', 'user-add-line', 'chat-new-line', 'code-box-line', 'menu-2-fill', 'file-list-3-line', 'user-add-line','home-2-line','user-3-line'],
+      ri: ['arrow-down-double-fill', 'arrow-down-circle-line', 'close-circle-line', 'mail-line', 'key-fill', 'menu-fill', 'add-box-line', 'file-copy-2-line', 'settings-5-line', 'user-add-line', 'chat-new-line', 'code-box-line', 'menu-2-fill', 'file-list-3-line', 'user-add-line','home-2-line','user-3-line','database-2-line','building-4-line','police-badge-line','global-line','signal-wifi-error-line'],
     }
   }), tailwind()]
 });
