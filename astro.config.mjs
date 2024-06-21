66cd3de184c117a034b067b04b0ece985ecd6001
Michael Price <michael@michamp.com>
2024-06-20 20:32:38 -0700

    Added site origin

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://glistening-brioche-48b229",
+
   // Enable React to support React JSX components.
   integrations: [react(), icon({
     include: {
