e259de71358dcf8c180334c2cd5d9960810d9740
Michael Price <michael@michamp.com>
2024-06-20 20:03:44 -0700

    Initial SWAT API V1 Template

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://glistening-brioche-48b229",
   // Enable React to support React JSX components.
   integrations: [react(), icon({
     include: {
@@ -14,7 +15,18 @@ export default defineConfig({
       // Include `carbon` icons in the bundle
       carbon: ['api-1', 'container-services', 'dashboard', 'app-connectivity', 'add', 'email', 'password', 'login', 'error', 'logout'],
       // Include `remix` icons in the bundle
-      ri: ['arrow-down-double-fill', 'arrow-down-circle-line', 'close-circle-line', 'mail-line', 'key-fill', 'menu-fill', 'add-box-line', 'file-copy-2-line', 'settings-5-line', 'user-add-line', 'chat-new-line', 'code-box-line', 'menu-2-fill', 'file-list-3-line', 'user-add-line','home-2-line','user-3-line','database-2-line','building-4-line','police-badge-line','global-line','signal-wifi-error-line','login-box-line','logout-box-line'],
+      ri: ['arrow-down-double-fill', 'arrow-down-circle-line', 'close-circle-line', 'mail-line', 'key-fill', 'menu-fill', 'add-box-line', 'file-copy-2-line', 'settings-5-line', 'user-add-line', 'chat-new-line', 'code-box-line', 'menu-2-fill', 'file-list-3-line', 'user-add-line','home-2-line','user-3-line','database-2-line','building-4-line','police-badge-line','global-line','signal-wifi-error-line','login-box-line','logout-box-line','run-fill',
+      'number-1',
+      'number-2',
+      'number-3',
+      'number-4',
+      'number-5',
+      'number-6',
+      'number-7',
+      'number-8',
+      'number-9'
+      
+      ],
     }
   }), tailwind()]
 });
---
+++ src/types/fbiTypes.ts
@@ -0,0 +1,86 @@
+// FBI Interfaces
+
+// FBI Types 
+export type FbiFileType =
+{
+	name:string;
+	url:string;
+
+};
+
+export type FbiCoordinateType = 
+{
+	lat:number;
+	formatted:string;
+	lng:number;
+
+};
+
+export type FbiImageType =
+{
+	original:string;
+	thumb:string;
+	large:string;
+	caption:string;
+};
+
+export type FbiType =
+{
+	dates_of_birth_used:Array<String>;
+	place_of_birth:string;
+	reward_text:string;
+	poster_classification:string;
+      	uid:string;
+	possible_states:Array<String>;
+      	sex:string;
+      	details:string;
+      	scars_and_marks:string;
+	images:Array<FbiImageType>;
+        reward_min:number;
+      	age_max:number;
+      	warning_message:string;
+      	weight:string;
+      	person_classification:string;
+      	url:string;
+      	ncic:string;
+      	race_raw:string;
+        weight_max:number;
+        nationality:string;
+        suspects:string;
+        remarks:string;
+      	race:string;
+        height_max:number;
+      	coordinates:Array<FbiCoordinateType>;
+      	title:string;
+      	aliases:string;
+      	eyes:string;
+      	publication:string;
+      	age_range:string;
+      	eyes_raw:string;
+        caution:string;
+      	path:string;
+      	complexion:string;
+      	hair_raw:string;
+      	legat_names:string;
+      	modified:string;
+      	locations:string;
+      	hair:string;
+      	age_min:number;
+      	languages:string;
+        status:string;
+      	field_offices:Array<String>;
+        files:Array<FbiFileType>;
+        subjects:Array<String>;
+      	possible_countries:Array<String>;
+      	occupations:Array<String>;
+      	build:string;
+      	height_min:number;
+      	weight_min:number;
+      	reward_max:number;
+      	additional_information:string;
+      	description:string;
+      	id:string;
+};
+
+
+
