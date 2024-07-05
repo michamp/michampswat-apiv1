85ad8d8c0886fb41375050fe6344f3ae2f30dbbb
Michael Price <michael@michamp.com>
2024-07-05 12:13:10 -0700

    Updated dialog for dashboard

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,23 +1,24 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import icon from "astro-icon";
-
 import tailwind from "@astrojs/tailwind";
+
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
   site: "http://localhost:4321",
-
   // Enable React to support React JSX components.
   integrations: [react(), icon({
     include: {
-    	      
       // Include `iconoir` icons in the bundle
-      iconoir: ['network-reverse', 'view-structure-down', 'cloud-sync', 'electronics-chip', 'database', 'code','pharmacy-squared-cross'],
+      iconoir: ['network-reverse', 'view-structure-down', 'cloud-sync', 'electronics-chip', 'database', 'code', 'pharmacy-squared-cross'],
       // Include `carbon` icons in the bundle
-      carbon: ['api-1', 'container-services', 'dashboard', 'app-connectivity', 'add', 'email', 'password', 'login', 'error', 'logout','caret-down'],
+      carbon: ['api-1', 'container-services', 'dashboard', 'app-connectivity', 'add', 'email', 'password', 'login', 'error', 'logout', 'caret-down'],
       // Include `remix` icons in the bundle
-      ri: ['arrow-down-circle-line','close-circle-line','mail-line','key-fill','menu-fill','add-box-line','file-copy-2-line','settings-5-line','user-add-line','code-box-line', 'menu-2-fill', 'file-list-3-line','user-add-line','home-2-line','user-3-line','database-2-line','login-box-line','logout-box-line','run-fill','folder-info-fill']
+      ri: ['arrow-down-circle-line', 'close-circle-line', 'mail-line', 'key-fill', 'menu-fill', 'add-box-line', 'file-copy-2-line', 'settings-5-line', 'user-add-line', 'code-box-line', 'menu-2-fill', 'file-list-3-line', 'user-add-line', 'home-2-line', 'user-3-line', 'database-2-line', 'login-box-line', 'logout-box-line', 'run-fill', 'folder-info-fill']
     }
-  }), tailwind()]
+  }), tailwind()],
+  output: "server",
+  adapter: netlify()
 });
