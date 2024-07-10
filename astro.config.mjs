fc174d36b2855b949e2bcd0963a3afd1b0997e1e
Michael Price <michael@michamp.com>
2024-07-09 20:22:18 -0700

    Added github

--- astro.config.mjs
+++ astro.config.mjs
@@ -14,7 +14,7 @@ export default defineConfig({
       // Include `iconoir` icons in the bundle
       iconoir: ['network-reverse', 'view-structure-down', 'cloud-sync', 'electronics-chip', 'database', 'code', 'pharmacy-squared-cross'],
       // Include `carbon` icons in the bundle
-      carbon: ['api-1', 'container-services', 'dashboard', 'app-connectivity', 'add', 'email', 'password', 'login', 'error', 'logout', 'caret-down'],
+      carbon: ['logo-github','api-1', 'container-services', 'dashboard', 'app-connectivity', 'add', 'email', 'password', 'login', 'error', 'logout', 'caret-down'],
       // Include `remix` icons in the bundle
       ri: ['arrow-down-circle-line', 'close-circle-line', 'mail-line', 'key-fill', 'menu-fill', 'add-box-line', 'file-copy-2-line', 'settings-5-line', 'user-add-line', 'code-box-line', 'menu-2-fill', 'file-list-3-line', 'user-add-line', 'home-2-line', 'user-3-line', 'database-2-line', 'login-box-line', 'logout-box-line', 'run-fill', 'folder-info-fill']
     }
